e061d9e0917d3777da3eefbfedb7538f1172d83e
NajibMuhammadi <najibmuhammadi003@gmail.com>
2025-10-03 14:46:21 +0200

    pushing

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ export const io = new Server(server, {
             "http://172.20.20.47:3000",
             "http://127.0.0.1:3000",
         ],
+        credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     },
 });
